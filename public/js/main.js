8f8cf2231db8ac948349a07c77fff438d4f5dfe3
jangyh07222 <jangyh07222@gmail.com>
2018-11-22 18:00:24 +0900

    181122_1

---
+++ public/js/main.js
@@ -0,0 +1,10 @@
+$(".ham_menu").click(function(){
+    $(".sub_menu").stop().animate({"left":300}, 700);
+    $(".main_txt").stop().animate({"margin-left": 300+"px"});
+    $(".top_menu").stop().animate({"opacity": 0});
+});
+$(".menu_close").click(function(){
+    $(".sub_menu").stop().animate({"left": -300}, 700);
+    $(".main_txt").stop().animate({"margin-left": 0 +"px"});
+    $(".top_menu").stop().delay(300).animate({"opacity": 1});
+});
