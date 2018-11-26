7f40f5b7d5f9990d68a5dee6db9e5465c3aaeea0
jangyh07222 <jangyh07222@gmail.com>
2018-11-26 17:10:00 +0900

    181126

--- public/js/main.js
+++ public/js/main.js
@@ -8,3 +8,9 @@ $(".menu_close").click(function(){
     $(".main_txt").stop().animate({"margin-left": 0 +"px"});
     $(".top_menu").stop().delay(300).animate({"opacity": 1});
 });
+
+$(".port .some").each("li").hover(function(){
+    $(this).attr("img").stop().animate({"opacity": 0.7}, 500)
+}, function(){
+    $(this).attr("img").stop().animate({"opacity": 0.8}, 500)
+});
